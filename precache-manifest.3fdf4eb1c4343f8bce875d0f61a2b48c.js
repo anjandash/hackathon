9ed26ad410a980627d3190ec9142233058223801
Anjan Karmakar <anjandash@Anjans-MacBook-Pro.local>
2019-11-15 11:41:10 +0100

    Updates

---
+++ precache-manifest.3fdf4eb1c4343f8bce875d0f61a2b48c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e68d26d87bc5b924433db35f4dbc0a22",
+    "url": "/hackathon/index.html"
+  },
+  {
+    "revision": "028be1d183070a2800b9",
+    "url": "/hackathon/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "19f21511392de8ce69da",
+    "url": "/hackathon/static/js/2.6c0f0d91.chunk.js"
+  },
+  {
+    "revision": "028be1d183070a2800b9",
+    "url": "/hackathon/static/js/main.10f2fc02.chunk.js"
+  },
+  {
+    "revision": "6afaf74af07e1b4fbc10",
+    "url": "/hackathon/static/js/runtime-main.636b7f7c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/hackathon/static/media/logo.25bf045c.svg"
+  }
+]);
